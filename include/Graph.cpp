a3e2cae28671ce8d273c9b74e31f1da4a7d8b42f
einan202 <q1ik1h55>
2020-11-09 16:43:59 +0200

    Constructor & GetEdges() ;

--- include/Graph.cpp
+++ include/Graph.cpp
@@ -1,43 +1,55 @@
 
-// Created by spl211 on 6/11/2020.
-#include "Graph.h"
-#include <fstream>
-using namespace std;
+// Last edited on 9/11/2020
 // Need to build Operation Function
 
-Graph::Graph(std::vector <std::vector<int>> matrix) {
+#include "Graph.h"
+#include <iostream>
+
+using namespace std;
+
+Graph:: Graph(vector<vector<int>> matrix):edges() { // Constructor
 
     for(vector<int> node : matrix) {
-
         vector<int> neighbors;
-        int counter  = 0;
-
-        for (int other : node){
-
+        int counter = 0;
+        for (int other : node) {
             if (other == 1)
-               neighbors.push_back(counter);
+                neighbors.push_back(counter);
             counter = counter + 1;
         }
-
         edges.push_back(neighbors);
     }
 }
 
-int main(int argv, char** argc){
+vector<vector<int>> Graph::getEdges() const{
+   return edges;
+}
 
-    vector<vector<int>> matrix = {{1},{0}};
-    std::cout << matrix << endl;
+void infectNode(int nodeInd){ // adding to Q
 
-    Graph(x);
+
+}
+
+bool isInfected(int nodeInd){ // boolean (how we know?)
+
+
+}
+
+int main() {
+    vector<vector<int>> matrix = {{0,1,0,0,1,0,0,0,0},{1,0,1,1,0,1,1,0,0},
+                                  {0,1,0,1,0,1,0,0,0},{0,1,1,0,0,1,0,0,0},
+                                  {1,0,0,0,0,1,0,0,0},{0,1,1,1,1,0,0,1,0},
+                                  {0,1,0,0,0,0,0,1,1},{0,0,0,0,0,1,1,0,0},
+                                  {0,0,0,0,0,0,1,0,0}};
+
+    Graph g(matrix);
+
+
+    for (int i = 0; i < g.getEdges().size(); ++i) {
+        for (int j = 0; j < g.getEdges()[i].size(); ++j) {
+        cout << g.getEdges()[i][j] << endl;
+    }
+
     return 0;
+    }
 }
-
-
-
-}
-
-
-}
-
-
-
--- include/Graph.h
+++ include/Graph.h
@@ -2,25 +2,27 @@
 #define GRAPH_H_
 
 #include <vector>
+#include <queue>
+
+// Copy Constructor (public)
+// Distructor (public)
 
 class Graph{
 public:
-    Graph(std::vector<std::vector<int>> matrix) {}; // Constructor
-    
-    // Copy Constructor
-    // Distructor
+
+    Graph(std::vector<std::vector<int>> matrix);
 
     void infectNode(int nodeInd); // adding to Q
     bool isInfected(int nodeInd); // boolean (how we know?)
 
-    /* Allel foult */
-    std::vector<std::vector<int>> getEdges(); // Getter for private field
-    std::vector<std::vector<int>> main();
+    std::vector<std::vector<int>> getEdges() const; // Getter for private field
+    int main();
 
 private:
     std::vector<std::vector<int>> edges;
-    /* Adding protected Q to use*/
-
+protected:
+    std::queue<int> Q;
+    std::vector<int> vertices; // **Update in Git**
 };
 
 #endif
